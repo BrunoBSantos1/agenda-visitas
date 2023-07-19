786accadd24c0922a3235ecfd0ee33d8f69c8fab
Bruno Batista Santos <brunobatysta.fla@gmail.com>
2023-07-19 11:14:09 -0300

    Criado componente de lista de visitas

---
+++ src/app/visitas/read-visitas/read-visitas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-read-visitas',
+  templateUrl: './read-visitas.component.html',
+  styleUrls: ['./read-visitas.component.css']
+})
+export class ReadVisitasComponent {
+
+}
