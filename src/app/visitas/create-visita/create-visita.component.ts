d40198610180bc5786e590a08a6fb5ac36d56c26
Bruno Batista Santos <brunobatysta.fla@gmail.com>
2023-07-20 14:31:47 -0300

    criando função cadastro

--- src/app/visitas/create-visita/create-visita.component.ts
+++ src/app/visitas/create-visita/create-visita.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ServiceService } from '../services/service.service';
+import { Visita } from '../models/visita-model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-create-visita',
@@ -9,6 +12,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 export class CreateVisitaComponent {
+  visita: Visita
+
   visitForm = new FormGroup({
     dateVisit: new FormControl(''),
     nameVisitor: new FormControl(''),
@@ -19,7 +24,29 @@ export class CreateVisitaComponent {
     feedbackVisit: new FormControl('')
   })
 
+  constructor(private visitService: ServiceService) {}
+
   onSubmitForm() {
     console.log(this.visitForm.value)
+    this.visita = {
+      dateVisit: new Date(),
+      nameVisitor: 'Bruno',
+      visitorOccupation: this.visitForm.value.visitorOccupation,
+      goalVisit: this.visitForm.value.goalVisit,
+      descriptionVisit: this.visitForm.value.descriptionVisit,
+      tourGuide: this.visitForm.value.tourGuide,
+      feedbackVisit: this.visitForm.value.feedbackVisit
+    }
+
+    this.conectionDb(this.visita)
+    
+  }
+
+  conectionDb(data: Visita) {
+    this.visitService.createVisita(data).subscribe(
+      (r) => {console.log(r)}),
+      (error) => {console.log(error)}
   }
 }
+
+
