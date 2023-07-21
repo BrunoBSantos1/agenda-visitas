10f3f48ab417cfa59de2ee5c288e2f280690f98d
Bruno Batista Santos <brunobatysta.fla@gmail.com>
2023-07-21 09:53:53 -0300

    Listando visitas

--- src/app/visitas/read-visitas/read-visitas.component.ts
+++ src/app/visitas/read-visitas/read-visitas.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../services/service.service';
+import { Observable, map, pipe } from 'rxjs';
+import { Visita } from '../models/visita-model';
 
 @Component({
   selector: 'app-read-visitas',
   templateUrl: './read-visitas.component.html',
   styleUrls: ['./read-visitas.component.css']
 })
-export class ReadVisitasComponent {
+export class ReadVisitasComponent implements OnInit {
 
+  allVisitas: Visita[];
+  constructor(private  visitaService: ServiceService) {}
+  
+  ngOnInit() {
+    this.allVisit()
+  }
+  
+  
+  allVisit() {
+    this.visitaService.readVisita().subscribe(data => {
+      this.allVisitas = data
+      console.log(this.allVisitas)
+    });
+  }
 }
