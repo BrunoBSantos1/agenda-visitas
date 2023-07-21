81142297b44b0764130294004ad0eac9d38c93df
Bruno Batista Santos <brunobatysta.fla@gmail.com>
2023-07-21 09:54:50 -0300

    conectando ao firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,12 @@ import { VisitasRoutingModule } from './visitas/visitas-routing.module';
 import { VisitasModule } from './visitas/visitas.module';
 
 
-import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 const configFirebase = environment.firebaseConfig
 
@@ -41,7 +45,11 @@ const configFirebase = environment.firebaseConfig
     MatListModule,
     MatButtonModule,
     MatCardModule,
-    VisitasModule
+    VisitasModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
     ],
   providers: [],
   bootstrap: [AppComponent]
