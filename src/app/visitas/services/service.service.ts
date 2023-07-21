ee410e5ad7f50cb658ab271192c56842469922f7
Bruno Batista Santos <brunobatysta.fla@gmail.com>
2023-07-21 09:54:17 -0300

    criando service para visitas

--- src/app/visitas/services/service.service.ts
+++ src/app/visitas/services/service.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Visita } from '../models/visita-model';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
-
+import { Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-    urlBase = environment.baseUrl
+  urlBase = collection(this.firestore, 'visitas');
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private firestore: Firestore
+    ) { }
 
-  createVisita(visita: Visita): Observable<Visita> {
-    return this.http.post<Visita>(`${this.urlBase}/visita.json`,visita)
+  createVisita(visita: Visita) {
+    return addDoc(this.urlBase, visita);
+  }
+
+  readVisita(): Observable<Visita[]> {
+    return collectionData(this.urlBase, {idField: 'id'}) as Observable<Visita[]>;
+    
   }
 }
